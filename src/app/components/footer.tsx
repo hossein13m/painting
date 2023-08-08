ecb8a4c7d617109e39d0ca53c58aadd5e25ffc9b
Hossein Mousavi <hossein13mousavi@gmail.com>
2023-08-08 13:10:21 +0330

    feat(footer): implement footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer(): JSX.Element {
+  return (
+    <footer className="flex justify-center pb-4">
+      <span className="mr-1">Created By:</span>
+      <a className="font-bold" href="https://hmousavi.dev/" target="_blank">
+        Hossein Mousavi
+      </a>
+    </footer>
+  );
+}
