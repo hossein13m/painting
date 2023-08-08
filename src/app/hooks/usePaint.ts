70ebc92d43b7fc468885bff70731d8f536f0f96b
Hossein Mousavi <hossein13mousavi@gmail.com>
2023-08-08 15:32:53 +0330

    feat(painting): implement canvas hook

---
+++ src/app/hooks/usePaint.ts
@@ -0,0 +1,63 @@
+import { useEffect, useRef, useState } from 'react';
+
+export type Point = Record<'x' | 'y', number>;
+
+export interface Paint {
+    ctx: CanvasRenderingContext2D;
+    currentPoint: Point;
+    prevPoint: Point | null;
+}
+
+export function usePaint(onPaint: ({ ctx, currentPoint, prevPoint }: Paint) => void) {
+    const [mouseDown, setMouseDown] = useState(false);
+
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const prevAxis = useRef<Point | null>(null);
+
+    function onMouseDown(): void {
+        setMouseDown(!mouseDown);
+    }
+
+    useEffect(() => {
+        function mouseMoveEventHandler(event: MouseEvent): void {
+            if (!mouseDown) return;
+            const currentAxis = setAxis(event);
+            const ctx = canvasRef.current?.getContext('2d');
+
+            if (!ctx || !currentAxis) return;
+
+            onPaint({ ctx, currentPoint: currentAxis, prevPoint: prevAxis.current });
+            prevAxis.current = currentAxis;
+        }
+
+        function mouseUpEventHandler() {
+            setMouseDown(!mouseDown);
+            prevAxis.current = null;
+        }
+
+        function setAxis(event: MouseEvent): Point | void {
+            const canvas = canvasRef.current;
+            if (!canvas) return;
+
+            const rect = canvas.getBoundingClientRect();
+            console.log(rect);
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+
+            return { x, y };
+        }
+
+        canvasRef.current?.addEventListener('mousemove', mouseMoveEventHandler);
+        window.addEventListener('mouseup', mouseUpEventHandler);
+
+        return () => {
+            canvasRef.current?.removeEventListener('mousemove', mouseMoveEventHandler);
+            window.removeEventListener('mouseup', mouseUpEventHandler);
+        };
+    }, [onPaint]);
+
+    return {
+        canvasRef,
+        onMouseDown,
+    };
+}
